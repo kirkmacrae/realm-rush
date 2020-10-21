289ccd85bcf816471b607d2daf26ccddb98bc866
Kirk MacRae <kirk.macrae@gmail.com>
2020-10-21 16:57:43 -0400

    coordinates on cubes

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
-    [SerializeField] [Range(1f,10f)] float gridSize = 10f;   
-    
+    [SerializeField] [Range(1f,10f)] float gridSize = 10f;
+    TextMesh textMesh;
+    private void Start()
+    {           
+    }
     // Update is called once per frame
     void Update()
     {
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + ", " + snapPos.z / gridSize;
     }
 }

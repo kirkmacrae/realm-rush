9b2064062f768a4b4f07d4f7b608032378a792e9
Kirk MacRae <kirk.macrae@gmail.com>
2020-10-21 21:59:13 -0400

    simple enemy movement

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PrintAllWaypoints());        
+    }
+
+    IEnumerator PrintAllWaypoints()
+    {
+        print("starting patrol...");
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            print("Visiting block: " + waypoint);
+            yield return new WaitForSeconds(1f);
+        }
+        print("ending patrol.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

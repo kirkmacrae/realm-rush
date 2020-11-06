2ba95d1347cddaf6a6dc5d66ddeb10adc82a06ce
Kirk MacRae <kirk.macrae@gmail.com>
2020-11-05 20:13:42 -0500

    spawn new enemies and target closest

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns = 5f;
+    [SerializeField] EnemyMovement enemyPrefab;
+    [SerializeField] Transform enemySpawnLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }   
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+            Instantiate(enemyPrefab, enemySpawnLocation.position, Quaternion.identity);
+        }
+    }
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,17 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using UnityEngine;
 
 public class Tower : MonoBehaviour    
 {
-    [SerializeField] Transform objectToPan;
-    [SerializeField] Transform targetEnemy;
+    [SerializeField] Transform objectToPan;    
     [SerializeField] float attackRange = 50f;
     [SerializeField] ParticleSystem projectileParticle;
+
+    Transform targetEnemy;
 
     // Update is called once per frame
     void Update()
     {
+        SetTargetEnemy();
+
         if (targetEnemy)
         {
             objectToPan.LookAt(targetEnemy);
@@ -21,7 +26,37 @@ public class Tower : MonoBehaviour
         {
             Shoot(false);
         }
-    }   
+    }
+
+    private void SetTargetEnemy()
+    {
+        var sceneEnemies = FindObjectsOfType<EnemyDamage>();
+        if(sceneEnemies.Length == 0) { return; }
+
+        Transform closestEnemy = sceneEnemies[0].transform;        
+        
+        foreach(EnemyDamage enemy in sceneEnemies)
+        {
+            closestEnemy = GetClosest(closestEnemy, enemy.transform);            
+        }
+
+        targetEnemy = closestEnemy;
+    }
+
+    private Transform GetClosest(Transform a, Transform b)
+    {
+        float distanceToA = Vector3.Distance(a.position, objectToPan.transform.position);
+        float distanceToB = Vector3.Distance(b.position, objectToPan.transform.position);
+
+        if (distanceToA <= distanceToB)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
 
     void ShootEnemy()
     {
@@ -43,5 +78,12 @@ public class Tower : MonoBehaviour
         var emissionModule = projectileParticle.emission;
         emissionModule.enabled = isActive;
      
+    }
+
+    private float getDistanceToEnemy(Transform enemy)
+    {
+        float distanceToEnemy = Vector3.Distance(enemy.position, objectToPan.transform.position);
+
+        return distanceToEnemy;
     }
 }
